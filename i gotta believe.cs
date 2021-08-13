37c1f97aed3a0c944df77b742e61f52487ff5a44
null301 <71235377+null301@users.noreply.github.com>
2021-08-13 07:16:54 +0200

    Actions have an effect now
    You can fail or win this now, it's still kind of not finished but, is this really supposed to be finished? No. But i want it to have actual interesting stuff in it so.

--- i gotta believe.cs
+++ i gotta believe.cs
@@ -9,17 +9,46 @@ namespace IDFK
             //Main variables that don't matter but do matter aaa shut UP
             int lives = 3;
             int score = 0;
+            string answer = "I gotta believe!";
             bool outOfLives = false;
+            string playerAnswer = ""; 
+            int guessCount = 0;       
+            //Asks the user for input and compares it
             
-            //Asks the user for input
-            Console.Write("You gotta do what?: ");
-            string playerAnswer = Console.ReadLine();
-   
-            //Compares the user's input and checks if it matches with correctAnswer
-            if (playerAnswer == "I gotta believe!")
-                Console.Write($"You gotta believe!\nYour score is now {++score}!");
+            while(playerAnswer != answer && !outOfLives)
+            {
+              if(guessCount < lives)
+              {
+                Console.Write("You gotta do what?: ");
+            playerAnswer = Console.ReadLine();
+             guessCount++;            
+              }
+              else 
+              {
+                outOfLives = true;
+              }
+              
+              
+            }
+            if(outOfLives)
+            {
+              Console.WriteLine($"You lost! \nYour score is {score}");
+              Console.WriteLine("To try again, type Retry.");
+              if(Console.ReadLine() == "Retry")
+              {
+                //Resets everything.
+                outOfLives = false;
+                guessCount = 0;
+                playerAnswer = "";
+              }
+                            
+              
+            } 
             else
-                Console.WriteLine($"Incorrect! You lost a life!\nYou have {--lives} lives left\nYour score is {score}");
+             {
+             Console.Write($"You gotta believe!\nYour score is now {++score}!");
+           
+              }         
         }
     } 
 }
