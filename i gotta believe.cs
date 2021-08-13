4e1b62b7575e52a28145f7f2c59b85236815cd79
null301 <71235377+null301@users.noreply.github.com>
2021-08-13 05:46:07 +0200

    Shit cleaned
    Yes

--- i gotta believe.cs
+++ i gotta believe.cs
@@ -2,41 +2,24 @@ using System;
 
 namespace IDFK
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      //Main variables that don't matter but do matter aaa shut UP
-      string correctAnswer = "I gotta believe!";
-       int lives = 3;
-       bool outOfLives = false;
-       int score = 0;
-       string playerAnswer = "null";
-   //Asks the user for input
-    Console.Write("You gotta do what?: ");
-    playerAnswer = Console.ReadLine();
-    //Compares the user's input and checks if it matches with correctAnswer
-    if (playerAnswer == correctAnswer)
-  {
-    //If it matches, it adds one point to the user's score
- score++;
-  Console.Write($"You gotta believe!\nYour score is now {score}!");
-  }
-  else
-   {
-     //If it doesn't, it removes one life.
-     lives--;
-     
-      Console.WriteLine($"Incorrect! You lost a life!\nYou have {lives} lives left");
-    Console.WriteLine($"Your score is {score}");
-    }
-    
-    }
-    
-    }
-    
-    
-    
+        static void Main(string[] args)
+        {
+            //Main variables that don't matter but do matter aaa shut UP
+            int lives = 3;
+            int score = 0;
+            bool outOfLives = false;
+            
+            //Asks the user for input
+            Console.Write("You gotta do what?: ");
+            string playerAnswer = Console.ReadLine();
    
-  }
-
+            //Compares the user's input and checks if it matches with correctAnswer
+            if (playerAnswer == "I gotta believe!")
+                Console.Write($"You gotta believe!\nYour score is now {++score}!");
+            else
+                Console.WriteLine($"Incorrect! You lost a life!\nYou have {--lives} lives left\nYour score is {score}");
+        }
+    } 
+}
