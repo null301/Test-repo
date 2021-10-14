05e25a87ee7eacea606a45cfbcec1293f5a4d611
null301 <71235377+null301@users.noreply.github.com>
2021-10-13 23:48:08 -0300

    Nickname thing
    It'll give you a nickname you can use on an alt or something temporal idk

---
+++ autnicknamer.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+ // Compiler version 4.0, .NET Framework 4.5
+
+
+ namespace boob
+ {
+   public class Program
+   {
+     public static void Main(string[] args)
+     {
+       //Words to use,in order.
+       string[] words1 = {"Alternative","Censored","Suppressed","Creative","Molten","Unusual", "Bound", "Toasty", "Classy", "Lazy", "Sentient", "Evil", "Responsible"};
+       string[] words2 = {"Kalashnikov","Puppet","Catwalk","Laxative","Lava","Earth","Monster", "Toaster", "Tuxedo", "Penguin", "Sandwich", "Bead", "Battery"};
+       
+       Random rnd = new Random();
+       
+       
+       int index = rnd.Next(0, 15);
+       
+       
+       Console.Write(words1[index]);Console.WriteLine(words2[index]);
+       
+     }
+   }
+ }
