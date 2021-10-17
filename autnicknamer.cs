b2c4d7ccd8ad5f9bd7b9818418e3e34a5eab222f
null301 <71235377+null301@users.noreply.github.com>
2021-10-16 22:58:37 -0300

    More variation
    no more repeated names every 3 uses, now it's only every 5!

--- autnicknamer.cs
+++ autnicknamer.cs
@@ -12,13 +12,13 @@ using System.Text.RegularExpressions;
      public static void Main(string[] args)
      {
        //Words to use,in order.
-       string[] words1 = {"Alternative","Censored","Suppressed","Creative","Molten","Unusual", "Bound", "Toasty", "Classy", "Lazy", "Sentient", "Evil", "Responsible"};
-       string[] words2 = {"Kalashnikov","Puppet","Catwalk","Laxative","Lava","Earth","Monster", "Toaster", "Tuxedo", "Penguin", "Sandwich", "Bead", "Battery"};
+       string[] words1 = {"Swedish","Laggy","Authentic","Alternative","Censored","Suppressed","Creative","Molten","Unusual", "Bound", "Toasty", "Classy", "Lazy", "Sentient", "Evil", "Responsible"};
+       string[] words2 = {"Slab","Landscape","Meatball","Octopus","Fossa","Kalashnikov","Puppet","Catwalk","Laxative","Lava","Earth","Monster", "Toaster", "Tuxedo", "Penguin", "Sandwich", "Bead", "Battery"};
        
        Random rnd = new Random();
        
        
-       int index = rnd.Next(0, 15);
+       int index = rnd.Next(0, 18);
        
        
        Console.Write(words1[index]);Console.WriteLine(words2[index]);
